ba9e84c8e7dd52a10a2a4519b7b5ad91660ba609
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-10-02 12:10:41 -0300

    Estilizando com tailwind utilizando plugins e configuração avançada

--- aula16/src/components/Menu/Menu.tsx
+++ aula16/src/components/Menu/Menu.tsx
@@ -2,10 +2,11 @@ import Link from "next/link";
 
 export default function Menu() {
   return (
-    <nav>
+    <nav className="menu">
         <ul>
             <li><Link href="/">Home</Link></li>
             <li><Link href="/produtos">Produtos</Link></li>
+            <li><Link href="/produtos/cad-produto">Cadastro</Link></li>
         </ul>
     </nav>
   )
